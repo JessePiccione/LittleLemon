1fd9300e5fe3b7fad4b07fc24c22f79c2f69123b
JessePiccione <jessepiccione@gmail.com>
2024-04-23 21:27:34 -0400

    set up backend api to power menu and booking systems

--- restaurant/models.py
+++ restaurant/models.py
@@ -5,7 +5,7 @@ class Booking(models.Model):
     Name = models.CharField(max_length=255)
     No_of_guests = models.SmallIntegerField()
     BookingDate = models.DateTimeField()
-class Menu(models.Model):
+class MenuItem(models.Model):
     Title = models.CharField(max_length=255)
     Price = models.DecimalField(max_digits=10, decimal_places=2)
     Inventory = models.SmallIntegerField()
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import *
+from .models import Booking, MenuItem
+from .serializers import BookingSerializer, MenuItemSerializer
+
+class MenuItemView(ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+

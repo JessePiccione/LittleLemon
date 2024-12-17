939b76405b386c1dafd51cdc748f21700631408f
Jesse Piccione <jessepiccione@yahoo.com>
2024-12-17 15:47:48 +0000

    now a shell react application. preparing for frontend developer capstone

--- restaurant/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from rest_framework.authtoken.views import obtain_auth_token
-urlpatterns = [
-    path('menu/items/', views.MenuItemView.as_view()),
-    path('menu/items/<int:pk>', views.SingleMenuItemView.as_view()),
-    path('api-token-auth', obtain_auth_token),
-    path('', views.index),
-]
